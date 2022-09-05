43ea08e06c5a50020329846be4aafc94074c709d
sapere4ude <eileenyoo2@gmail.com>
2022-09-05 22:50:39 +0900

    - Hello Subject

--- 02.Publishers-and-Subscribers/Publishers-and-Subscribers.playground/Contents.swift
+++ 02.Publishers-and-Subscribers/Publishers-and-Subscribers.playground/Contents.swift
@@ -237,6 +237,81 @@ example(of: "CurrentValueSubject") {
     // 2
     let subject = CurrentValueSubject<Int, Never>(0)
     
+    // 3
+    subject
+        .print()
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions) // 4
+    
+    subject.send(1)
+    subject.send(2)
+    print("subject.value:\(subject.value)")
+    subject.send(3)
+    print("subject.value:\(subject.value)")
+    
+    subject
+      .sink(receiveValue: { print("Second subscription:", $0) })
+      .store(in: &subscriptions)
+
+    subject.send(completion: .finished)
 }
+
+example(of: "Dynamically adjusting Demand") {
+  final class IntSubscriber: Subscriber {
+    typealias Input = Int
+    typealias Failure = Never
+    
+    func receive(subscription: Subscription) {
+      subscription.request(.max(2))
+    }
+    
+    func receive(_ input: Int) -> Subscribers.Demand {
+      print("Received value", input)
+      
+      switch input {
+      case 1:
+        return .max(2) // 1
+      case 3:
+        return .max(1) // 2
+      default:
+        return .none // 3
+      }
+    }
+    
+    func receive(completion: Subscribers.Completion<Never>) {
+      print("Received completion", completion)
+    }
+  }
+  
+  let subscriber = IntSubscriber()
+  
+  let subject = PassthroughSubject<Int, Never>()
+  
+  subject.subscribe(subscriber)
+  
+  subject.send(1)
+  subject.send(2)
+  subject.send(3)
+  subject.send(4)
+  subject.send(5)
+  subject.send(6)
+}
+
+example(of: "Type erasure") {
+  // 1
+  let subject = PassthroughSubject<Int, Never>()
+  
+  // 2
+  let publisher = subject.eraseToAnyPublisher()
+  
+  // 3
+  publisher
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+  
+  // 4
+  subject.send(0)
+}
+
 
 

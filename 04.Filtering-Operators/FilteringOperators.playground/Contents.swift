a841e59ddff54f48736493b080be68239a125cf5
sapere4ude <eileenyoo2@gmail.com>
2022-09-12 17:01:12 +0900

    - compactMap - ignoreOutput - first(where:)

--- 04.Filtering-Operators/FilteringOperators.playground/Contents.swift
+++ 04.Filtering-Operators/FilteringOperators.playground/Contents.swift
@@ -24,3 +24,72 @@ example(of: "removeDuplicates") {
         .sink(receiveValue: { print($0) })
         .store(in: &subscriptions)
 }
+
+example(of: "compactMap") {
+    let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
+    
+    strings
+        .compactMap { Float($0) }
+        .sink(receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
+
+let possibleNumbers = ["1", "2", "three", "///4///", "5"]
+
+let mapped: [Int?] = possibleNumbers.map { str in Int(str) }
+// [1, 2, nil, nil, 5]
+
+let compactMapped: [Int] = possibleNumbers.compactMap { str in Int(str) }
+// [1, 2, 5]
+
+
+example(of: "ignoreOutput") {
+  // 1
+  let numbers = (1...10_000).publisher
+  
+  // 2
+  numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: { print("Completed with: \($0)") },
+          receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "first(where:)") {
+    // 1
+    let numbers = (1...9).publisher
+    
+    // 2
+    numbers
+        .print("numbers")
+        .first(where: { $0 % 2 == 0 })
+        .sink(receiveCompletion: { print("Completed with: \($0)") },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    // practice
+//    numbers
+//        .first(where: { $0 * 3 == 9 })
+//        .sink(receiveCompletion: { print("completed with: \($0)") },
+//              receiveValue: { print($0 )})
+//        .store(in: &subscriptions)
+}
+
+example(of: "last(where:)") {
+    let numbers = PassthroughSubject<Int, Never>()
+    
+    numbers
+        .last(where: { $0 % 2 == 0 })
+        .sink(receiveCompletion: { print("Completed with: \($0)") },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    numbers.send(1)
+    numbers.send(2)
+    numbers.send(3)
+    numbers.send(4)
+    numbers.send(5)
+    
+    numbers.send(completion: .finished)
+}
+

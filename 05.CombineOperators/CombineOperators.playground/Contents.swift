4567071b962fb0b7956bc8bac643781a47977871
sapere4ude <eileenyoo2@gmail.com>
2022-09-14 08:37:48 +0900

    - prepend(Output) - prepend(Sequence) - prepend(Publisher) - append(Output) - append(Sequence) - append(Publisher)

---
+++ 05.CombineOperators/CombineOperators.playground/Contents.swift
@@ -0,0 +1,115 @@
+import UIKit
+import Combine
+
+var subscriptions = Set<AnyCancellable>()
+
+example(of: "prepend(Output...)") {
+  // 1
+  let publisher = [3, 4].publisher
+  
+  // 2
+  publisher
+    .prepend(1, 2)
+    .prepend(-1, 0)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prepend(Sequence)") {
+  // 1
+  let publisher = [5, 6, 7].publisher
+  
+  // 2
+  publisher
+    .prepend([3, 4])
+    .prepend(Set(1...2))
+    .prepend(stride(from: 6, to: 11, by: 2))
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher)") {
+  // 1
+  let publisher1 = [3, 4].publisher
+  let publisher2 = [1, 5].publisher
+  
+  // 2
+  publisher1
+    .prepend(publisher2) // [1,5] 값이 prefix로 들어간다는걸 생각해야함
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher) #2") {
+  // 1
+  let publisher1 = [3, 4].publisher
+  let publisher2 = PassthroughSubject<Int, Never>()
+  
+  // 2
+  publisher1
+    .prepend(publisher2)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+
+  // 3
+  publisher2.send(1)
+  publisher2.send(2)
+    
+  publisher2.send(completion: .finished) // publisher2의 emit 완료가 진행되어야만 publisher1 값이 emit 된다.
+}
+
+example(of: "append(Output...)") {
+  // 1
+  let publisher = [1].publisher
+
+  // 2
+  publisher
+    .append(2, 3)
+    .append(4)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "append(Output...) #2") {
+  // 1
+  let publisher = PassthroughSubject<Int, Never>()
+
+  publisher
+    .append(3, 4)
+    .append(5)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+  
+  // 2. PassthroughSubject를 사용했기 때문에 .send 가 우선순위가 있는 상태이고 이게 끝났다는걸 .finished 로 명시해야만 append(3,4), append(5) 가 동작할 수 있다.
+  publisher.send(1)
+  publisher.send(2)
+  publisher.send(completion: .finished)
+}
+
+example(of: "append(Sequence)") {
+// As you can see, the execution of the appends is sequential as the previous publisher must complete before the next append performs.
+    
+  // 1
+  let publisher = [1, 2, 3].publisher
+    
+  publisher
+    .append([4, 5]) // 2
+    .append(Set([6, 7])) // 3
+    .append(stride(from: 8, to: 11, by: 2)) // 4
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+example(of: "prepend(Publisher)") {
+  // 1
+  let publisher1 = [3, 4].publisher
+  let publisher2 = [1, 2].publisher
+  
+  // 2
+  publisher1
+    .prepend(publisher2)    // publisher2 send .finished 가 되어야만 publisher1이 event를 emit 할 수 있다.
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+}
+
+

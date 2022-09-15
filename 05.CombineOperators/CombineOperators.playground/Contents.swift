ef5ffadd3796bd17cafcbfec4eb2dfd5893fccaa
sapere4ude <eileenyoo2@gmail.com>
2022-09-15 23:47:45 +0900

    - prepend(Publisher) #2

--- 05.CombineOperators/CombineOperators.playground/Contents.swift
+++ 05.CombineOperators/CombineOperators.playground/Contents.swift
@@ -112,4 +112,23 @@ example(of: "prepend(Publisher)") {
     .store(in: &subscriptions)
 }
 
+example(of: "prepend(Publisher) #2") {
+  // 1
+  let publisher1 = [3, 4].publisher
+  let publisher2 = PassthroughSubject<Int, Never>()
+  
+  // 2
+  publisher1
+    .prepend(publisher2)
+    .sink(receiveValue: { print($0) })
+    .store(in: &subscriptions)
+
+  // 3
+  publisher2.send(1)
+  publisher2.send(2)
+    
+  publisher2.send(completion: .finished)
+
+}
+
 

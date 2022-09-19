985e8a9170861d3ee0d8e31d1e9b1e01f5ba8edc
kant <eileenyoo2@gmail.com>
2022-09-19 18:33:38 +0900

    - merge(with:) - combineLatest

--- 05.CombineOperators/CombineOperators.playground/Contents.swift
+++ 05.CombineOperators/CombineOperators.playground/Contents.swift
@@ -131,4 +131,64 @@ example(of: "prepend(Publisher) #2") {
 
 }
 
+example(of: "merge(with:)") {
+  // 1
+  let publisher1 = PassthroughSubject<Int, Never>()
+  let publisher2 = PassthroughSubject<Int, Never>()
+
+  // 2
+  publisher1
+    .merge(with: publisher2)
+    .sink(
+      receiveCompletion: { _ in print("Completed") },
+      receiveValue: { print($0) }
+    )
+    .store(in: &subscriptions)
+
+  // 3
+  publisher1.send(1)
+  publisher1.send(2)
+
+  publisher2.send(3)
+
+  publisher1.send(4)
+
+  publisher2.send(5)
+
+  // 4
+  publisher1.send(completion: .finished)
+  publisher2.send(completion: .finished)
+}
+
+
+example(of: "combineLatest") {
+  // 1
+  let publisher1 = PassthroughSubject<Int, Never>()
+  let publisher2 = PassthroughSubject<String, Never>()
+
+  // 2
+  publisher1
+    .combineLatest(publisher2)
+    .sink(
+      receiveCompletion: { _ in print("Completed") },
+      receiveValue: { print("P1: \($0), P2: \($1)") }
+    )
+    .store(in: &subscriptions)
+
+  // 3
+  publisher1.send(1)
+  publisher1.send(2)
+  
+  publisher2.send("a")
+  publisher2.send("b")
+  
+  publisher1.send(3)
+  
+  publisher2.send("c")
+
+  // 4
+  publisher1.send(completion: .finished)
+  publisher2.send(completion: .finished)
+}
+
 
